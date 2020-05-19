75534c85ef21610f9a245787e2fb85f52303798b
jeff_simon <jeffsbillinfo@gmail.com>
2020-05-19 14:17:53 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>        
+        {movies.map((movie,movieIndex) => (
+          <div key={movieIndex}>
+            <h3>Movie: {movie.title}</h3>
+            <h4>Running time:{movie.time}</h4>
+            <p>Genres:</p>
+            <ul>{movie.genres.map((genre,genreIndex) => 
+            (<li key={genreIndex}>{genre}</li>))}</ul></div>
+        ))}
     </div>
   );
 };
